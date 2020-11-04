d8fec0537c487062cd3c755c7a0262e2c0c8c029
Zack <lewisxzk@outlook.com>
2020-11-04 13:42:13 +0800

    7.3 finished. Waiting test...

--- Test7-3/MyVector.h
+++ Test7-3/MyVector.h
@@ -37,9 +37,8 @@ private:
 template <typename T>
 MyVector<T>::MyVector(T* arr, int len){
     if(len <= 0) throw exception("length error");
-    this->m_elems = new T[len];
-    while(len >= m_max) this->resize(this->m_max * 2);     // len in [1/2, 1] of m_size
-    while(len <= m_max/4) this->resize(this->m_max / 2);
+    delete this->m_elems;
+    this->m_elems = new T[len * 2];
     for(int i=0;i<len;i++) {
         this->m_elems[i] = arr[i];
         ++this->m_size;
@@ -94,10 +93,63 @@ T& MyVector<T>::pop_back(){
     return item;
 }
 template <typename T>
-void MyVector<T>::resize(int capacity){
+void MyVector<T>::insert(T item, int loc) {
+    if(loc > m_size - 1) throw exception("Out of range!");
+    if(m_size == m_max) resize(2 * m_max);    // 先判断满，再自增
+    for(int i = m_size-1;i > loc; --i) {
+        m_elems[i+1] = m_elems[i];
+    }
+    m_elems[loc] = item;
+    ++m_size;
+}
+template <typename T>
+void MyVector<T>::erase() {
+    if(isEmpty()) throw exception("Vector Empty!");
+    if(loc < 0 || loc > m_size - 1) throw exception("Out of range!");
+    for(int i = loc;i < m_size;++i) {
+        m_elems[i] = m_elems[i+1];
+    }
+    --m_size;
+    if(m_size == m_max/4) resize(m_max/2);
+}
+template <typename T>
+void MyVector<T>::clear() {     // 内存也清空, m_size置2
+    delete m_elems;
+    T* m_elems = new T[2];
+    m_max = 2;
+    m_size = 0;
+}
+template <typename T>
+void MyVector<T>::sort() {
+    for(int i = 0; i < m_size - 1; ++i) {
+        for(int j = 0; j < m_size -1 -i; ++j) {
+            if(m_elems[j] > m_elems[j+1]) {
+                T temp = m_elems[j+1];
+                m_elems[j+1] = m_elems[j];
+                m_elems[j] = temp;
+            }
+        }
+    }
+}
+template <typename T>
+int MyVector<T>::binary_search(T item) {
+    this->sort();
+    int left = 0;
+    int right = m_size - 1;
+    int middle;
+    while(left <= right) {
+        middle = (left + right) / 2;
+        if(m_elems[middle] == item) return middle;
+        else if ( m_elems[middle] > item) right = middle - 1;
+        else left = middle + 1;
+    }
+    return -1;  // 查找失败
+}
+template <typename T>
+void MyVector<T>::resize(int capacity) {
     if(capacity <= 0) throw exception("Range Error: capacity can't be zero or negative!");
     T* copy = new T[capacity];
-    for(int i = 0;i < this->m_size;i++) {
+    for(int i = 0;i < this->m_size;++i) {
         copy[i] = this->m_elems[i];
     }
     delete this->m_elems;
--- Test7-3/main.cpp
+++ Test7-3/main.cpp
@@ -1,6 +1,11 @@
 #include "MyVector.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    std::cout << "Hello, Test7.3!" << std::endl;
+    int inputV[3] = {1, 2, 3};
+    MyVector<int> v0;       // 默认构造
+    MyVector<int> v1(inputV, 3);     // 带参构造
+    MyVector<int> v2(v1);      // 复制构造
+    MyVector<int> v3 = v2;  // 赋值
+    cout << "v3 size:\t" << v3.size();
 }
